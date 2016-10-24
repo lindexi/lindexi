634f5965741eeaad754dec34159d9e5e4b57fc66
lindexi_gd <lindexi_gd@163.com>
2016-10-24 16:17:25 +0800

    2016年10月24日16:17:24

--- js/all.js
+++ js/all.js
@@ -26,14 +26,14 @@ $(document).ready(function(){
 
 		$("<span/>").html("<div class='ds-thread' data-category='<#article/category/id#>' data-thread-key='<#article/id#>' data-title='<#article/title#>' data-author-key='<#article/author/id#>'></div>").appendTo('#clear');
 		var duoshuoQuery = { short_name: "lindexi" };
-		(function () {
-		    var ds = document.createElement('script');
-		    ds.type = 'text/javascript'; ds.async = true;
-		    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		    ds.charset = 'UTF-8';
-		    (document.getElementsByTagName('head')[0]
-             || document.getElementsByTagName('body')[0]).appendChild(ds);
-		})();
+		//(function () {
+		//    var ds = document.createElement('script');
+		//    ds.type = 'text/javascript'; ds.async = true;
+		//    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		//    ds.charset = 'UTF-8';
+		//    (document.getElementsByTagName('head')[0]
+        //     || document.getElementsByTagName('body')[0]).appendChild(ds);
+		//})();
 
 	}
 	
