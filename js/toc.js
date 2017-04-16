8129b16d5e06e769611a7d42cbf256ab7c8e6609
lindexi <lindexi@cvte.com>
2017-04-16 14:37:41 +0800

    2017年4月16日 14:37:40

--- js/toc.js
+++ js/toc.js
@@ -3,7 +3,7 @@
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>目录</i>',
+      title: '目录',
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
